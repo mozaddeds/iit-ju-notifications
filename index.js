d0fce2b5164e07c8fb18d17ebf7c869575c77ec7
Mozadded-Ul-Islam <pmozadded@gmail.com>
2022-03-28 11:07:04 +0600

    env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mozaddeds:mozadded.09@cluster0.7rq6e.mongodb.net/iit-ju-update?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7rq6e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
